ff02affde5b472c0d01d4829487044687270dc79
danyaal uddin <danyaalu@gmail.com>
2025-07-18 12:48:22 +0100

    Created 'FilleManager' class and implemented base function for saving tasks to file

---
+++ Classes/FileManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Task_Manager.Classes
+{
+    internal class FileManager
+    {
+        public static List<TaskItem> LoadFile()
+        {
+            List<TaskItem> tasks = new List<TaskItem>();
+
+
+
+            return tasks;
+        }
+        public static bool SaveFile(List<TaskItem> tasks)
+        {
+            string json = JsonSerializer.Serialize(tasks, new JsonSerializerOptions { WriteIndented = true });
+            try
+            {
+                using (StreamWriter sw = new StreamWriter("data/tasks.json"))
+                {
+                    sw.Write(json);
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
